45bcd3c831d1b26d790da12097551208c41e090a
Shreya Rajwade <s4.shre@gmail.com>
2019-03-12 09:32:48 -0400

    Update settings.py

--- django_rest_framework_api/settings.py
+++ django_rest_framework_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j97qa%-=q0)fu9-07xng0k1(*^ywap3j-50=s8td-#y%5^1u%t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
